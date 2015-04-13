b7dd93e391e287a4d410a09e097702a0b4e8876a
Jack Low <jacklow@gmail.com>
2015-04-13 22:14:21 +1000

    Initial commit

---
+++ app/controllers/ScaleController.scala
@@ -0,0 +1,61 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+import play.api.libs.json.Json
+import play.api.Routes
+
+case class Note(pitchClass: String, octave: Int)
+case class Scale(notes: Seq[Note])
+
+object ScaleController extends Controller {
+
+  implicit val noteWrites = Json.writes[Note]
+  implicit val scaleWrites = Json.writes[Scale]
+
+  def getScale = Action { implicit request =>
+  	val root = request.queryString.get("root")
+  	root match {
+  		case Some(_) => Ok(Json.toJson(Scale(List(Note(root.get.head, 3)))))
+  		case _ => BadRequest("please provide root")
+  	}
+  }
+
+  def getTranspose = Action { implicit request =>
+	val note = request.queryString.get("note")
+	val step = request.queryString.get("step")
+
+	Ok(Json.toJson(transpose(Note(note.get.head, 3), step.get.head.toInt)))
+  }
+
+  def transpose(note: Note, step: Int): Note = {
+  	pitch(absPitch(note) + step)
+  }
+
+  def absPitch(note: Note): Int = {
+  	12 * note.octave + pitchClassToInt(note.pitchClass)
+  }
+
+  def pitchClassToInt(pitchClass: String): Int = {
+  	pitchClass match {
+  		case "C" => 0
+  		case "Cs" => 1
+  		case "D" => 2
+  		case "Ds" => 3
+  		case "E" => 4
+  		case "F" => 5
+  		case "Fs" => 6
+  		case "G" => 7
+  		case "Gs" => 8
+  		case "A" => 9
+  		case "As" => 10
+  		case "B" => 11
+  	}
+  }
+
+  def pitch(absPitch: Int): Note = {
+  	val pitch = absPitch % 12
+  	val octave = absPitch / 12
+  	Note(List("C","Cs","D","Ds","E","F","Fs","G","Gs","A","As","B")(pitch), octave)
+  }
+
+}
